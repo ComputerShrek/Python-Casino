869349a9a695766d9533bb3671192a675c9202bf
ComputerSchrek <larg-graffiti@outlook.de>
2025-06-21 17:22:02 +0200

    Create coinflip.py

---
+++ coinflip.py
@@ -0,0 +1,21 @@
+import random
+
+def game():
+    x = random.randint(1. 2)
+    if x == 1:
+        print("Head")
+    else:
+        print("Tails")
+
+while True:
+    play = input("What do you bet on H/T \n")
+    if play == H
+        bet = 1
+    else:
+        bet = 2
+    game()
+    if x == bet:
+        print("You won!")
+    else:
+        print("You lost!")
+        print("Now play fap roulette you loser!")
